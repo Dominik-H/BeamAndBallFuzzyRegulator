8d7a2baa5205c8a5d7f61e7b44713f7dae4eb740
Dominik-H <hornak.dominik@gmail.com>
2015-11-22 14:25:47 +0100

    PhysicalModel

--- BeamAndBallApp/BeamAndBallApp/PhysicalModel.cpp
+++ BeamAndBallApp/BeamAndBallApp/PhysicalModel.cpp
@@ -3,10 +3,36 @@
 
 
 PhysicalModel::PhysicalModel()
+	:gravity(0.0f, -10.0f), world(gravity)
 {
 }
 
 
 PhysicalModel::~PhysicalModel()
 {
+}
+
+bool PhysicalModel::Init(int width, int height, float servoTimeDelay)
+{
+	return true;
+}
+
+void PhysicalModel::Update(int dt)
+{
+
+}
+
+void PhysicalModel::SetServoTimeDelay(float servoTimeDelay)
+{
+
+}
+
+void PhysicalModel::SetGravity(float gravityAcceleration)
+{
+
+}
+
+bool PhysicalModel::ReInit()
+{
+	return true;
 }
--- BeamAndBallApp/BeamAndBallApp/PhysicalModel.h
+++ BeamAndBallApp/BeamAndBallApp/PhysicalModel.h
@@ -8,5 +8,27 @@ class PhysicalModel
 public:
 	PhysicalModel();
 	~PhysicalModel();
+
+	bool Init(int width, int height, float servoTimeDelay);
+	void Update(int dt);
+	void SetServoTimeDelay(float servoTimeDelay);
+	void SetGravity(float gravityAcceleration);
+
+private:
+	bool ReInit();
+
+	FuzzyRegulator regulator;
+	float regulatorOutput;
+	float servoTimeDelay;
+	b2World world;
+	b2Vec2 gravity;
+	bool initialized;
+	std::vector<b2Body*> walls;
+	std::vector<b2PolygonShape> wallShapes;
+	std::map<std::string, b2Body*> bAndBBodies;
+	b2PolygonShape beam;
+	b2PolygonShape connector;
+	b2CircleShape ball;
+	b2CircleShape servo;
 };
 
