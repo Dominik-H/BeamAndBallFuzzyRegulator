e4d32ee8e79708a274328edf64e4e584326daf9c
Dominik-H <hornak.dominik@gmail.com>
2015-12-02 18:27:32 +0100

    Tralala :)

--- BeamAndBallApp/BeamAndBallApp/Application.cpp
+++ BeamAndBallApp/BeamAndBallApp/Application.cpp
@@ -78,7 +78,7 @@ bool Application::Init(sf::RenderWindow* window)
 
 void Application::Update(sf::Time dt) 
 {
-	physicalWorld.Update(dt.asSeconds(), 5.0f);
+	physicalWorld.Update(dt.asSeconds(), 0.25f);
 	std::map<std::string, b2Body*>* bodies = physicalWorld.GetBodies();
 	/*
 	// Update code...
